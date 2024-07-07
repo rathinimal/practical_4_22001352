80f64d8a24ed4cb9434293ab15f77bddbb99febf
rathinimal <rathinarasanimal@gmail.com>
2024-07-08 01:23:51 +0530

    Q2 is adding

--- Q1.scala
+++ Q1.scala
@@ -8,6 +8,10 @@ object inventory_management {
         var stock = Array(20,60,20,10)
     
         displayInventory(item.length-1)
+        restockitem("Pen",20)
+        sellitem("Pencil",5)
+        restockitem("book",30)
+        sellitem("ruler",5)
 
 
         def displayInventory(x:Int): Any = x match {
@@ -18,10 +22,29 @@ object inventory_management {
         
         }
 
-        def restockitem(item:Array[String],y:Int): Any {
-             stock(y) = stock(y)+y
-             println(s"The new stock of $item()")
+        def restockitem(w:String,y:Int): Any = {
+            val string_to_find = w
+            if (item.contains(string_to_find)){
+                val index_of_string = item.indexOf(string_to_find)
+                stock(index_of_string) += y
+                println(s"The new stock of $w: ${stock(index_of_string)}")
+            }else {
+                println("stock is not found in list.")
+            }
+        }
+        def sellitem(w1:String,y1:Int):Any = {
+            val string_to_find_1 = w1
+            if (item.contains(string_to_find_1)){
+                val index_of_string = item.indexOf(string_to_find_1)
+                stock(index_of_string) -= y1
+                println(s"The new stock of $w1: ${stock(index_of_string)}")
+            }else {
+                println("stock is not found in list.")
+            }
         }
 
-}
+            
+
+        }
+
 }
