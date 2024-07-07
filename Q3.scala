32fe97876831be6f35c6db4ad9c0a2e453a446a7
rathinimal <rathinarasanimal@gmail.com>
2024-07-08 02:44:33 +0530

    updated

---
+++ Q3.scala
@@ -0,0 +1,18 @@
+object letter {
+    def main(args: Array[String]): Unit ={
+        toUpper("Benny")
+        toLower("Saman")
+
+
+        def toUpper(w1:String): Any ={
+            println(s"${w1.toUpperCase()}")
+        }
+
+        def toLower(w2:String): Any = {
+            println(s"${w2.toLowerCase()}")
+        }
+
+        //def formatNames()
+    }
+
+}
