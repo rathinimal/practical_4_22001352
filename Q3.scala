3e60330775901184dc17b843688c20fcfeaa81d9
rathinimal <rathinarasanimal@gmail.com>
2024-07-08 16:07:33 +0530

    Qusetion 3 is completed

--- Q3.scala
+++ Q3.scala
@@ -1,7 +1,9 @@
 object letter {
     def main(args: Array[String]): Unit ={
         toUpper("Benny")
+        formatNames("Nirsoan")
         toLower("Saman")
+        formatNames1("Kumara")
 
 
         def toUpper(w1:String): Any ={
@@ -12,7 +14,15 @@ object letter {
             println(s"${w2.toLowerCase()}")
         }
 
-        //def formatNames()
+        // for Niroshan output NIroshan
+        def formatNames(w3:String): Any ={
+            println(w3.substring(0,2).toUpperCase() + w3.substring(2).toLowerCase())
+        }
+
+        // for Kumara output KumarA
+        def formatNames1(w4: String): Any ={
+            println(w4.substring(0,1).toUpperCase() + w4.substring(1,5) + w4.substring(5).toUpperCase())
+        }
     }
 
 }
